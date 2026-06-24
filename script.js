ce0063b1dcbcb12b045c899b04d070def25037be
Amuthanprasanna <amuthanprasanna@thestackly.com>
2026-06-24 10:48:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Task 1 - Employee Name
+let employeeName = prompt("Please enter your name:");
+
+console.log("Employee Name:", employeeName);
+
+document.writeln("Welcome " + employeeName);
+
+
+
